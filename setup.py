15543ec2d81e9927fc14a38403d408883c3e9258
Sutol <117520656+SUT0L@users.noreply.github.com>
2025-04-29 10:49:15 +1000

    Exclude example directory

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ setup(
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/SUT0L/unofficial-dehashed-api/",
-    packages=find_packages(),
+    packages=find_packages(exclude=["examples", "examples.*"]),
     classifiers=[
         "Programming Language :: Python :: 3",
         "Programming Language :: Python :: 3.10",
